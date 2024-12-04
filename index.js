89cbd172b01d2eddb0448819834b91d03e703ce4
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-12-04 23:26:47 +0600

    added delete method

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/gameReviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const quary = { _id: new ObjectId(id) };
+      const result = await gameReviewCollection.deleteOne(quary);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
