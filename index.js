2cafc8552197769240555ec56803fbee888e1a56
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-12-07 18:56:13 +0600

    added recap game delete

--- index.js
+++ index.js
@@ -86,12 +86,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/gameReviews/:id", async (req, res) => {
-      const id = req.params.id;
-      const quary = { _id: new ObjectId(id) };
-      const result = await gameReviewCollection.deleteOne(quary);
-      res.send(result);
-    });
+    
 
     // MY WATCH LIST COLLECTION!
 
